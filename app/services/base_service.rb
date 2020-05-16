957847127db230b8b0a49509b558e36572c8a48b
Bas Donker <bdonker@nudgerewards.com>
2020-05-16 10:15:28 -0400

    Implement base service

---
+++ app/services/base_service.rb
@@ -0,0 +1,29 @@
+class BaseService
+  class Error < StandardError; end
+
+  attr_reader :params
+
+  def initialize(params = {})
+    @params = params ? params.dup.to_h.with_indifferent_access : {}.with_indifferent_access
+  end
+
+  def execute
+    raise NotImplementedError, '"execute" method missing'
+  end
+
+  protected def error(message, http_status = nil)
+    result = {
+        message: message,
+        status: :error
+    }
+
+    result[:http_status] = http_status if http_status
+    result
+  end
+
+  protected def success(pass_back = {})
+    pass_back[:status] = :success
+
+    pass_back
+  end
+end
