a3aaf792c2cd8b5d997db2e1d5cc804428799e2b
Bas Donker <bdonker@nudgerewards.com>
2020-05-16 15:02:45 -0400

    Configure devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, stretches: 13
+         :recoverable, :rememberable, :validatable,
+         :confirmable, stretches: 13
 end
