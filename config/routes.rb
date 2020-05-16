c3b1ef57e4c08c549ce44252ce60f580ee43b9cf
Bas Donker <bdonker@nudgerewards.com>
2020-05-16 15:02:45 -0400

    Configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
+
+  resources :users
 
   root 'home#index'
 end
