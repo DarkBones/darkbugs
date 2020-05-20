114a3defb05078baa010134d0b65960efdcfa2f3
Bas Donker <bdonker@nudgerewards.com>
2020-05-20 16:47:55 -0400

    Implement i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,24 @@ module ApplicationHelper
 
     data
   end
+
+  def css_page_class
+    controller_name = 'controller-' + params[:controller].tr('/', '-').tr('_', '-')
+    page_name = 'page-' + params[:action].tr('/', '-').tr('_', '-')
+
+    class_names = []
+    class_names << controller_name
+    class_names << page_name
+
+    class_names.join(' ')
+  end
+
+  def t(path)
+    I18n.t "#{i18n_path_prefix}.#{path}"
+  end
+
+  private def i18n_path_prefix
+    url = request.path.gsub '/', '.'
+    "views#{url}"
+  end
 end
