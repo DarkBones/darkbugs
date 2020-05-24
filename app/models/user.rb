7d774312fd92c47636deef9e4c05d85008d1254c
Bas Donker <bdonker@nudgerewards.com>
2020-05-24 13:18:42 -0400

    Create user profile model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, stretches: 13
+
+  # -- Relationships --------------------------------------------------------
+
+  has_one :user_profile, dependent: :destroy
 end
---
+++ app/models/user_profile.rb
@@ -0,0 +1,6 @@
+class UserProfile < ApplicationRecord
+  # -- Relationships --------------------------------------------------------
+
+  belongs_to :user
+
+end
