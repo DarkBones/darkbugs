3b35a63b6bb5ec68a135e7633455c5f9248bf0a4
Bas Donker <bdonker@nudgerewards.com>
2020-05-24 08:57:44 -0400

    Configure mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,28 +4,33 @@ class UserMailer < PostageApp::Mailer
     SENDER_INFO = "#{ENV['APP_NAME']} <info@#{ENV['APP_NAME'].downcase}.com>".freeze
   ].freeze
 
-  default from: SENDER_NO_REPLY
   include Devise::Mailers::Helpers
+  layout 'mailer'
 
-  def confirmation_instructions(record, _token, _opts={})
-    postageapp_template 'user_confirmation'
-    postageapp_variables :title => 'Confirm Your Account',
-                         :name => record.email ||= record.email,
-                         :link => user_confirmation_url(confirmation_token: record.confirmation_token)
-    mail(to: record.email)
+  default template_path: 'user_mailer/'
+  default from: SENDER_NO_REPLY
+
+  def confirmation_instructions(record, token, _opts = {})
+    @title = I18n.t('mailers.user_mailer.confirmation_instructions.title')
+    @name = record.email
+    @link = user_confirmation_url(confirmation_token: token)
+    mail(to: record.email,
+         subject: @title)
   end
 
-  def reset_password_instructions(record, _token, _opts = {})
-    @title = 'this is a test title'
-    @test = 'this is a test test'
-    mail(to: record.email)
+  def reset_password_instructions(record, token, _opts = {})
+    @title = I18n.t('mailers.user_mailer.reset_password_instructions.title')
+    @name = record.email
+    @link = edit_user_password_url(record, reset_password_token: token)
+    mail(to: record.email,
+         subject: @title)
   end
 
-  def unlock_instructions(record)
-    postageapp_template 'unlock_instructions'
-    postageapp_variables :title => 'Unlock Your Account',
-                         :name => record.name ||= record.email,
-                         :link => unlock_url(:unlock_token => record.unlock_token)
-    mail(to: record.email)
+  def unlock_instructions(record, token, _opts = {})
+    @title = I18n.t('mailers.user_mailer.unlock_instructions.title')
+    @name = record.email
+    @link = user_unlock_url(:unlock_token => token)
+    mail(to: record.email,
+         subject: @title)
   end
 end
--- app/mailers/user_mailer_local.rb
+++ app/mailers/user_mailer_local.rb
@@ -1,11 +1,31 @@
 class UserMailerLocal < Devise::Mailer
   include Devise::Mailers::Helpers
+  layout 'mailer'
+
   default template_path: 'user_mailer/'
   default from: UserMailer::SENDER_NO_REPLY
 
-  def reset_password_instructions(record, _token, _opts = {})
-    @title = 'this is a test title local'
-    @test = 'this is a test test local'
-    mail(to: record.email)
+  def confirmation_instructions(record, token, _opts = {})
+    @title = I18n.t('mailers.user_mailer.confirmation_instructions.title')
+    @name = record.email
+    @link = user_confirmation_url(confirmation_token: token)
+    mail(to: record.email,
+         subject: @title)
+  end
+
+  def reset_password_instructions(record, token, _opts = {})
+    @title = I18n.t('mailers.user_mailer.reset_password_instructions.title')
+    @name = record.email
+    @link = edit_user_password_url(record, reset_password_token: token)
+    mail(to: record.email,
+         subject: @title)
+  end
+
+  def unlock_instructions(record, token, _opts = {})
+    @title = I18n.t('mailers.user_mailer.unlock_instructions.title')
+    @name = record.email
+    @link = user_unlock_url(:unlock_token => token)
+    mail(to: record.email,
+         subject: @title)
   end
 end
