a36ecd925b422e3ffb5cafeb5dd4a10a77143669
Bas Donker <bdonker@nudgerewards.com>
2020-05-24 11:36:26 -0400

    Create tests

---
+++ test/controllers/users/confirmations_controller_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+module Users
+  class ConfirmationsControllerTest < ActionController::TestCase
+    def setup
+      @user = users(:default)
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+    end
+
+    def test_new
+      get :new
+      assert_response :success
+      assert_template :new
+    end
+
+    def test_create_blank
+      post :create
+      assert_response :success
+      assert_includes response.body, "Email can&#39;t be blank"
+      assert_template :new
+    end
+
+    def test_create_already_confirmed
+      params = { user: { email: @user.email } }
+
+      post :create, params: params
+      assert_response :success
+      assert_not_includes response.body, "Email can&#39;t be blank"
+      assert_includes response.body, 'Email was already confirmed, please try signing in'
+      assert_template :new
+    end
+
+    def test_create
+      user = users(:unconfirmed)
+      params = { user: { email: user.email } }
+
+      post :create, params: params
+      assert_response :redirect
+      assert_not_includes response.body, "Email can&#39;t be blank"
+    end
+  end
+end
