2dc07813199558b1ddf916607d78647548420ec7
Bas Donker <bdonker@nudgerewards.com>
2020-05-26 18:42:09 -0400

    Refactor forms
    Bug fixes
    
    Setup forms
    
    Setup forms
    
    Bug fixes
    
    Setup forms
    
    Setup forms

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'unauthenticated_blank', only: [:new, :create]
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params
 
   # GET /resource/sign_up
   # def new
@@ -42,9 +41,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, user_profile_attributes: [:username]])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
