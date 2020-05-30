59ef2fb4adc624c3c0b53c44b9d247a810500ab9
Bas Donker <bdonker@nudgerewards.com>
2020-05-30 09:11:44 -0400

    Bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,10 @@ class User < ApplicationRecord
          :confirmable, :lockable, stretches: 13
 
   # -- Relationships --------------------------------------------------------
-
   has_one :user_profile, dependent: :destroy
   accepts_nested_attributes_for :user_profile
 
   # -- Validations --------------------------------------------------------
-
   validates_presence_of :email
   validates_presence_of :password
   validates_presence_of :password_confirmation
