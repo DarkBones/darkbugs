e7aa352c2c307bd92aae0bae8b6fce409cf3dfa8
Bas Donker <bdonker@nudgerewards.com>
2020-06-21 13:56:14 -0400

    Install axios

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   }
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
+  resources :uploads, only: [:create]
   resources :users
 
   root 'home#index'
