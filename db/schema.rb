7dde2fdfcac2cfedfaa6e5aa4ac3a02c25fb3f17
Bas Donker <bdonker@nudgerewards.com>
2020-06-22 18:39:28 -0400

    Revert "Remove old controller"
    This reverts commit 3cebe091ce68a471c6d9eccce01916db4335a6a2.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_205836) do
+ActiveRecord::Schema.define(version: 2020_06_21_161048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2020_06_21_205836) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "uploads", force: :cascade do |t|
+    t.string "owner_table"
+    t.integer "owner_id"
+    t.string "owner_field"
+    t.string "path"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "user_profiles", force: :cascade do |t|
