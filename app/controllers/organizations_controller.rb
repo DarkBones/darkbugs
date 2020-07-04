2b3cf1b784b9af3cba155dded7730ea03734d8ab
Bas Donker <bdonker@nudgerewards.com>
2020-07-04 17:04:08 -0400

    Create organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,10 +1,23 @@
 class OrganizationsController < ApplicationController
+  respond_to :html
+  before_action :build_organization, only: %i[new create]
+
   def create
     Organization.create!(create_or_update_params)
+  rescue ActiveRecord::RecordInvalid => e
+    flash.now[:error] = e.message
+    render action: :new
+  end
+
+  def new; end
+
+  private def build_organization
+    @organization = Organization.new(create_or_update_params)
+    @organization.validate if params[:organization].present?
   end
 
   private def create_or_update_params
-    params.fetch(:organization).permit(
+    params.fetch(:organization, {}).permit(
       :name
     )
   end
--- test/controllers/organizations_controller_test.rb
+++ test/controllers/organizations_controller_test.rb
@@ -12,6 +12,24 @@ class OrganizationsControllerTest < ActionController::TestCase
       }
     }
 
+    organization = Organization.last
+
     assert_response :success
+    assert_equal 'new organization', organization.name
+    assert_equal 'new-organization', organization.slug
+  end
+
+  def test_create_fail_duplicate
+    2.times do
+      post :create, params: {
+        organization: {
+          name: 'test duplicate name'
+        }
+      }
+    end
+
+    assert_response :success
+    assert_template :new
+    assert_includes response.body, 'Name is already taken'
   end
 end
