afd58b5e9d791b27a54f888dd9d73cc97fe0deb5
Bas Donker <bdonker@nudgerewards.com>
2020-07-05 16:29:50 -0400

    Enable subdomains

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_user
   after_action :clear_flash
+  before_action :load_organization
 
   private def set_user
     @user = current_user
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
 
   private def clear_flash
     flash.discard if request.xhr?
+  end
+
+  private def load_organization
+    if request.subdomain.present?
+      @org = Organization.find_by!(slug: request.subdomain)
+    else
+      @org = current_user
+    end
   end
 end
