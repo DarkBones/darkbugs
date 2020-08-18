7e4fd5608549dcdccf79c291b7f2f57d3339cf71
Bas Donker <bdonker@nudgerewards.com>
2020-08-18 08:19:24 -0400

    New version

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     unlocks: 'users/unlocks'
   }
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  mount LetsEncrypt::Engine => '/.well-known' if Rails.env.production?
 
   post 'uploads/user_avatar' => 'uploads#create_user_avatar'
   delete 'uploads/user_avatar' => 'uploads#delete_user_avatar'
