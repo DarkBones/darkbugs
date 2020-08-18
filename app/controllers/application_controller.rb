103b94338e96f3ed7d716197205ec4f06dceae64
Bas Donker <bdonker@nudgerewards.com>
2020-08-17 22:15:08 -0400

    Bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protect_from_forgery prepend: true
+  # protect_from_forgery prepend: true
   skip_before_action :verify_authenticity_token
   before_action :set_user
   after_action :clear_flash
