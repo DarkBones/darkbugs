17b2b4e56258d1bf9381e019589e7744d8227bbe
Bas Donker <bdonker@nudgerewards.com>
2020-08-22 18:42:24 -0400

    Bug fixes

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -4,7 +4,7 @@ class UserProfile < ApplicationRecord
   has_one_attached :avatar
 
   # -- Validations --------------------------------------------------------
-  validates_presence_of :username
+  validates :username, presence: true
   validates :username, format: /\A[a-z0-9\-_]+\z/i, allow_blank: true
   validates :username, uniqueness: { case_sensitive: false }
 end
