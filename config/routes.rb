a6bc59fa34b43d6c021cc28d5e5b2d5279debbd8
Bas Donker <bdonker@nudgerewards.com>
2020-08-23 12:09:56 -0400

    Link to profile

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
   put '/organizations/:slug/revoke_admin/:user_uuid',   to: 'organizations#revoke_admin',   as: 'organization_revoke_admin'
   delete '/organizations/:slug/remove_member/:user_uuid',  to: 'organizations#remove_member',  as: 'organization_remove_member'
 
-  get '/user/:username', to: 'user_profiles#show'
+  get '/user/:username', to: 'user_profiles#show', as: 'user_profile'
 
   root 'home#index'
 end
