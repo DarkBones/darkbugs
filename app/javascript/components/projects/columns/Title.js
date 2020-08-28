6feb681044ce2a30631af59c6646d88a0891f300
Bas Donker <bdonker@nudgerewards.com>
2020-08-28 16:30:54 -0400

    Edit title

--- app/javascript/components/projects/columns/Title.js
+++ app/javascript/components/projects/columns/Title.js
@@ -1,16 +1,6 @@
 import React from 'react'
 
 export default class Title extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h4 className='column-title' onClick={this.handleClick}>
-          {this.state.title}
-        </h4>
-      </React.Fragment>
-  )
-  }
-
   constructor(props) {
     super(props)
 
@@ -20,7 +10,36 @@ export default class Title extends React.Component {
     }
   }
 
-  handleClick = () => {
-    alert('EDIT TITLE')
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick);
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      isEditing: this.node.contains(e.target)
+    })
+  }
+
+  render() {
+    const title = (
+      <h4 className='column-title' onClick={this.handleClick}>
+        {this.state.title}
+      </h4>
+    )
+
+    const titleElement = this.state.isEditing
+      ? <input className='form-control' value={this.state.title} />
+      : title
+
+
+    return (
+      <div ref={node => this.node = node}>
+        {titleElement}
+      </div>
+    )
   }
 }
