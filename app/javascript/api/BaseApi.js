9978dd022972df74d68008191f616357869cfb55
Bas Donker <bdonker@nudgerewards.com>
2020-08-28 18:34:25 -0400

    Secure api calls

--- app/javascript/api/BaseApi.js
+++ app/javascript/api/BaseApi.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 export const BASE_URL = '/api/v1'
 
+const csrfToken = document.querySelector("meta[name=csrf-token]").content
+axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
 export function get(path, params = {}) {
   const endpoint = `${BASE_URL}${path}`
 
