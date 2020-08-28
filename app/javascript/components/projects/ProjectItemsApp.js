f9bb31eecd0d459c228730d0dfbd8d4ff3573c64
Bas Donker <bdonker@nudgerewards.com>
2020-08-28 15:12:59 -0400

    Style columns

--- app/javascript/components/projects/ProjectItemsApp.js
+++ app/javascript/components/projects/ProjectItemsApp.js
@@ -3,6 +3,10 @@ import React from 'react'
 export default class ProjectItemsApp extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      columns: props.columns
+    }
   }
 
   render() {
@@ -12,13 +16,18 @@ export default class ProjectItemsApp extends React.Component {
           {this.props.title}
         </h1>
         <div id='columns'>
-          {this.props.columns.map((column) =>
+          {this.state.columns.map((column) =>
             <div className='column rounded p-2 pb-5'>
               <h4 className='column-title'>
                 {column.title}
               </h4>
             </div>
           )}
+          <div className='add-column-btn-container'>
+            <div className='add-column-btn clickable'>
+              <i className='fa fa-plus-circle fa-3x'/>
+            </div>
+          </div>
         </div>
       </div>
     )
