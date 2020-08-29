7957219d0b3d91a9a519ba23fe2109b074737815
Bas Donker <bdonker@nudgerewards.com>
2020-08-29 15:26:58 -0400

    Fix style issues

--- app/javascript/components/projects/columns/Title.js
+++ app/javascript/components/projects/columns/Title.js
@@ -4,18 +4,22 @@ import DeleteColumnButton from "./DeleteColumnButton";
 
 export default function Title(props) {
   return (
-    <React.Fragment>
-      <Name
-        name={props.name}
-        column_uuid={props.uuid}
-        isNew={props.uuid === ''}
-        cancelNewColumns={props.cancelNewColumns}
-        saveNewColumn={props.saveNewColumn}
-        boardSlug={props.boardSlug}
-      />
-      <DeleteColumnButton
-        handleClick={props.handleDeleteClick}
-      />
-    </React.Fragment>
+    <div className='row'>
+      <div className='col-10'>
+        <Name
+          name={props.name}
+          column_uuid={props.uuid}
+          isNew={props.uuid === ''}
+          cancelNewColumns={props.cancelNewColumns}
+          saveNewColumn={props.saveNewColumn}
+          boardSlug={props.boardSlug}
+        />
+      </div>
+      <div className='col-2'>
+        <DeleteColumnButton
+          handleClick={props.handleDeleteClick}
+        />
+      </div>
+    </div>
   )
 }
