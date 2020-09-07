f063d8ddee38b47a2e2b6bb2ccef10fc6e558dde
Bas Donker <bdonker@nudgerewards.com>
2020-09-07 14:49:36 -0400

    Reorder cards

--- app/services/cards/set_positions_service.rb
+++ app/services/cards/set_positions_service.rb
@@ -13,18 +13,33 @@ module Cards
     end
 
     private def set_positions
-      # Setting this at 100,000 and working backwards avoids
-      # having to update all card positions every time a new
-      # one is created (this assumes old cards don't get moved much)
-      position = 100_000
+      previous_card = nil
+      board.ordered_columns.each do |col|
+        col.cards.ordered.each do |card|
+          if previous_card.nil?
+            card.prepend
+          else
+            card.append_to(previous_card)
+          end
 
-      board.ordered_columns.reverse.each do |col|
-        col.cards.order(:position).reverse.each do |c|
-          c.update!(position: position) if c.position != position
-
-          position -= 1
+          previous_card = card
         end
       end
     end
+
+    # private def set_positions
+    #   # Setting this at 100,000 and working backwards avoids
+    #   # having to update all card positions every time a new
+    #   # one is created (this assumes old cards don't get moved much)
+    #   position = 100_000
+    #
+    #   board.ordered_columns.reverse.each do |col|
+    #     col.cards.order(:position).reverse.each do |c|
+    #       c.update!(position: position) if c.position != position
+    #
+    #       position -= 1
+    #     end
+    #   end
+    # end
   end
 end
