e727fea71a7c9a03d66cdad94cb6c2468f4f5f83
DarkBones <donkerbc@gmail.com>
2020-10-18 14:13:34 +0200

    Refactor

--- app/javascript/components/boards/card_modal/item/Item.js
+++ app/javascript/components/boards/card_modal/item/Item.js
@@ -1,4 +1,5 @@
 import React      from 'react'
+import Form       from '../../../shared/input/Form'
 import Note       from './Note'
 import PropTypes  from 'prop-types'
 
@@ -6,24 +7,67 @@ export default class Item extends React.Component {
   constructor(props) {
     super(props)
 
+    this.formParams = this.getFormParams()
+
     this.state = {
       isEditing: props.item.uuid === 'new'
+    }
+  }
+
+  handleCancel = () => {
+    console.log('cancel')
+  }
+
+  handleSubmit = params => {
+    console.log(params)
+  }
+
+  getFormParams = () => {
+    const { params, type } = this.props.item
+
+    switch (type) {
+      case 'note':
+        return {
+          fieldOrder: ['content'],
+          fields: {
+            content: {
+              name: 'content',
+              type: 'text',
+              value: params.content
+            }
+          }
+        }
     }
   }
 
   getItem = () => {
     const { params, type, uuid } = this.props.item
 
+    if (this.state.isEditing) return this.itemForm()
+
     switch (type) {
       case 'note':
         return (
           <Note
-            isEditing={this.state.isEditing}
             params={params}
             uuid={uuid}
           />
         )
     }
+  }
+
+  itemForm = () => {
+    const { handleCancel, handleSubmit } = this
+    const { fieldOrder, fields } = this.formParams
+
+    return (
+      <Form
+        fieldOrder={fieldOrder}
+        fields={fields}
+        handleCancel={handleCancel}
+        handleSubmit={handleSubmit}
+      />
+    )
   }
 
   render() {
--- app/javascript/components/boards/card_modal/item/Note.js
+++ app/javascript/components/boards/card_modal/item/Note.js
@@ -2,86 +2,26 @@ import React from 'react'
 import Form from '../../../shared/input/Form'
 import PropTypes from 'prop-types'
 
-export default class Note extends React.Component {
-  constructor(props) {
-    super(props)
-
-    const content = props.params.content
-
-    this.formParams = {
-      fieldOrder: ['content'],
-      fields: {
-        content: {
-          name: 'content',
-          type: 'text',
-          value: content
-        }
-      }
-    }
-
-    this.state = {
-      content: content
-    }
+export default function Note(props) {
+  const noteElement = () => {
+    return props.params.content
   }
 
-  handleCancel = () => {
-    console.log('handle cancel')
-  }
+  const { uuid } = props
+  const note = noteElement()
 
-  handleOnChange = e => {
-    console.log('handle on change')
-  }
-
-  handleSubmit = params => {
-    console.log(params)
-  }
-
-  noteContent = () => {
-    return this.state.content
-  }
-
-  noteElement = () => {
-    const { noteContent, noteForm } = this
-
-    return this.props.isEditing
-      ? noteForm()
-      : noteContent()
-  }
-
-  noteForm = () => {
-    const { handleCancel, handleSubmit } = this
-    const { fieldOrder, fields } = this.formParams
-
-    return (
-      <Form
-        fieldOrder={fieldOrder}
-        fields={fields}
-        handleCancel={handleCancel}
-        handleSubmit={handleSubmit}
-      />
-    )
-  }
-
-  render() {
-    const { noteElement } = this
-
-    const { uuid } = this.props
-    const note = noteElement()
-
-    return (
-      <div
-        itemType="note"
-        id={uuid}
-        className="display-linebreak"
-      >
-        {note}
-      </div>
-    )
-  }
+  return (
+    <div
+      itemType="note"
+      id={uuid}
+      className="display-linebreak"
+    >
+      {note}
+    </div>
+  )
 }
 
 Note.propTypes = {
-  isEditing:  PropTypes.bool.isRequired,
   params:     PropTypes.object.isRequired,
   uuid:       PropTypes.string.isRequired
 }
