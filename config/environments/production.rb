74eec40dc8b6569345aa03da7411dd3fb683558a
DarkBones <donkerbc@gmail.com>
2020-11-06 14:54:31 +0100

    Add hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,8 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.hosts << /([a-z0-9\-_]+\.)*darkbugs\.com/i
-  # config.hosts << /^10\.[0-9]{2}.[0-9]+\.[0-9]{2}$/i
+  config.hosts << /^10\.[0-9]{2}.[0-9]+\.[0-9]{2}$/i
+  config.hosts << 'localhost'
 
   Raven.configure do |config|
     config.dsn = Rails.application.credentials.sentry_url
