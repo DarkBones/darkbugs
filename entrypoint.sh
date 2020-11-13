52e3d44bf9c7f1c63f8c59df36d491f1217003c3
DarkBones <donkerbc@gmail.com>
2020-11-13 11:34:21 +0100

    Update crontab

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /darkbugs/tmp/pids/server.pid
 
+# Update crontab
+bundle exec whenever --update-crontab
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
