d3df9c551d6a738dd31a0d664230b7ea384ad2a1
DarkBones <donkerbc@gmail.com>
2020-11-14 13:45:13 +0100

    Deligate board creation to service

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,15 +2,26 @@ class Board < ApplicationRecord
   include Slugable
   include Assignable
 
+  # -- Constants ------------------------------------------------------------.
+  DEFAULT_COLUMNS = %w[
+    open
+    in_progress
+    done
+    released
+    archived
+  ]
+
+  # -- Relationships --------------------------------------------------------
   has_many :columns, dependent: :destroy
   has_many :cards, -> { order(position: :asc) }, dependent: :destroy
   belongs_to :component, polymorphic: true
   belongs_to :root_project, class_name: :Project, foreign_key: :root_project_id
 
+  # -- Validations ----------------------------------------------------------
   validates_presence_of :name
 
+  # -- Instance Methods -----------------------------------------------------
   def user_is_assigned?(user)
     component.user_is_assigned?(user)
   end
-
 end
